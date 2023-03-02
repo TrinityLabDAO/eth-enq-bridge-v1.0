4a8eb3e79d0a338b89a9570699915d852000a227
Mikhail Vasilev <floyddd-vm@mail.ru>
2023-03-02 10:42:15 +0400

    deploy scripts (goerli mumbai bsc)

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,50 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-etherscan");
 
+const PRIVATE_KEY = <>;
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.7",
+  networks: {
+    goerli: {
+      url: `https://goerli.infura.io/v3/88762928a4164487b3fcdd2dc892598d`,
+      accounts: [PRIVATE_KEY],
+      id: 5
+    },
+    polygonMumbai: {
+      url: "https://rpc-mumbai.maticvigil.com",
+      accounts: [PRIVATE_KEY],
+      id: 80001
+    },
+    bscTestnet: {
+      url: `https://data-seed-prebsc-1-s1.binance.org:8545`,
+      accounts: [PRIVATE_KEY],
+      id: 97
+    }
+  },
+  etherscan: {
+    apiKey: {
+      goerli: "IVD4ZVQ8Z2VU613SHF9FNS4ABKKF7WPDYK",
+      bscTestnet : 'G6FS6SET5P7V7B6KGFHTTGAFY8Q62FI72H',
+      polygonMumbai : 'NRSIR1KMP51HM6S2XXAAI2V867G6SUS2DV'
+    },
+    customChains: [
+      {
+        network: "goerli",
+        chainId: 5,
+        urls: {
+          apiURL: "https://api-goerli.etherscan.io/api",
+          browserURL: "https://goerli.etherscan.io"
+        }
+      },
+      {
+        network: "polygonMumbai",
+        chainId: 80001,
+        urls: {
+          apiURL: "https://api-testnet.polygonscan.com",
+          browserURL: "https://mumbai.polygonscan.com"
+        }
+      }
+    ]
+  }
 };
---
+++ scripts/deploy_polygonMumbai.js
@@ -0,0 +1,44 @@
+async function main() {
+    const [deployer] = await ethers.getSigners();
+
+    console.log("Deploying contracts with the account:", deployer.address);
+
+    console.log("Account balance:", (await deployer.getBalance()).toString());
+
+    const SpaceVault = await ethers.getContractFactory("SpaceVault");
+    const SpaceStorage = await ethers.getContractFactory("SpaceStorage");
+    const SpaceBridge = await ethers.getContractFactory("SpaceBridge");
+    // To deploy our contract, we just have to call Token.deploy() and await
+    // its deployed() method, which happens once its transaction has been
+    // mined.
+    const hardhatVault = await SpaceVault.deploy();
+    const hardhatStorage = await SpaceStorage.deploy();
+    const hardhatBridge = await SpaceBridge.deploy(80001);
+
+    await hardhatVault.setBridge(hardhatBridge.address);
+    await hardhatStorage.setBridge(hardhatBridge.address);
+    //BIT network
+    let networkId = 1;
+    let decimals = 10;
+    await hardhatStorage.addNetwork(networkId, decimals);
+    await hardhatStorage.addNetwork(5, 18); //goerli
+    await hardhatStorage.addNetwork(97, 18); //bsc
+
+    await hardhatStorage.addValidator("0x1F04445E17AA4B64cc9390fd2f76474A5e9B72c1");
+
+    await hardhatStorage.setThreshold(1);
+
+    await hardhatBridge.setVault(hardhatVault.address);
+    await hardhatBridge.setStorage(hardhatStorage.address);
+
+    console.log("Vault address:", hardhatVault.address);
+    console.log("Storage address:", hardhatStorage.address);
+    console.log("Bridge address:", hardhatBridge.address);
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
